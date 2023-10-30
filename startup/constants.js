ac4570a38ad12cf93c5761e71c98b7f7265f2fde
Footer <71946346+FooterMan15@users.noreply.github.com>
2023-10-30 11:36:55 -0400

    Create constants.js

---
+++ startup/constants.js
@@ -0,0 +1,30 @@
+//priority: 10
+//Script must have high priority, otherwise colors may load too late.
+
+//Minecraft
+const copper = 0xc15a36
+const iron = 0xD8D8D8
+const gold = 0xFDF55F
+//Immersive Engineering
+const tin = 0x465d6b
+const silver = 0x83aab2
+const lead = 0x453d5e
+const nickel = 0xd8b87d
+const steel = 0x43464b
+const aluminum = 0xa2a8ab
+const electrum = 0xE3C739
+const constantan = 0xF5A280
+//Mekanism
+const osmium = 0x67869f
+const bronze = 0xD07C1B
+//Create
+const zinc = 0x96b685
+const brass = 0xE9BA54
+//Thermal Series
+const enderium = 0x1C7AA0
+const lumium = 0xfffd7e
+const invar = 0x87928c
+const signalum = 0xe4340f
+//Custom made by FooterMan
+const tungsten = 0xB5FF9F
+const signar = 0xc37e48
