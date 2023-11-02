3d6a226eb396c009e4cb329a47d3b9104da387d8
Footer <71946346+FooterMan15@users.noreply.github.com>
2023-11-02 10:37:27 -0400

    Create raw_ore.js

---
+++ startup/raw_ore.js
@@ -0,0 +1,33 @@
+//Creates Raw Ore for non-Vanilla and non-Alloy materials.
+const raw_ore_properties = {
+  tin: { color: tin },
+  silver: { color: silver },
+  lead: { color: lead },
+  nickel: { color: nickel },
+  aluminum: { color: aluminum },
+  osmium: { color: osmium },
+  uranium: { color: osmium },
+  zinc: { color: zinc }
+}
+
+global.raw_ores = [
+  'tin',
+  'silver',
+  'lead',
+  'nickel',
+  'aluminum',
+  'osmium',
+  'uranium',
+  'zinc'
+]
+
+StartupEvents.registry('item', event => {
+  for (const mat of global.raw_ores) {
+    event
+      .create(`raw_${mat}`)
+      .texture('layer0', 'kubejs:item/raw_ore')
+      .color(0, raw_ore_properties[mat].color)
+      .tag('forge:raw_materials')
+      .tag(`forge:raw_materials/${mat}`)
+  }
+})
